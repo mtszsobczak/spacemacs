d7f59451f46a585fc5955c0fd9fe06cbe9bdd32b
Mateusz Sobczak <mtszsobczak@gmail.com>
2017-03-12 08:58:30 +0100

    Fix typescript tsx files lint

--- layers/+lang/typescript/packages.el
+++ layers/+lang/typescript/packages.el
@@ -71,16 +71,29 @@
 
 (defun typescript/post-init-web-mode ()
   (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
-  ;; FIXME -- this is not good!
-  (add-hook 'web-mode-hook
-            (lambda ()
-              (when (and (buffer-file-name)
-                         (string-equal "tsx" (file-name-extension (buffer-file-name))))
-                (tide-setup)
-                (flycheck-mode +1)
-                (eldoc-mode +1)
-                (when (configuration-layer/package-usedp 'company)
-                  (company-mode-on))))))
+  (defun setup-tide-mode ()
+    (interactive)
+    (tide-setup)
+    (flycheck-mode +1)
+    (setq flycheck-check-syntax-automatically '(save mode-enabled))
+    (eldoc-mode +1)
+    (tide-hl-identifier-mode +1)
+    ;; company is an optional dependency. You have to
+    ;; install it separately via package-install
+    ;; `M-x package-install [ret] company`
+    (company-mode +1)
+    (spacemacs|add-company-backends
+      :backends company-tide
+      :modes web-mode)
+
+    (flycheck-add-next-checker 'tsx-tide '(warning . typescript-tslint) 'append)
+    (flycheck-add-mode 'typescript-tslint 'web-mode)
+    )
+
+    (add-hook 'web-mode-hook
+              (lambda ()
+                (when (string-equal "tsx" (file-name-extension buffer-file-name))
+                  (setup-tide-mode)))))
 
 (defun typescript/init-typescript-mode ()
   (use-package typescript-mode
